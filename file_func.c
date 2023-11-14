a03e084f26585d566bc455d805c0ce185f2ac677
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-14 11:45:12 -0800

    beginning

--- file_func.c
+++ file_func.c
@@ -3,6 +3,11 @@
  * readf - reads a file and parses it
  * @fd: pointer to the file descriptor of the file to read
  */
-void readf()
+void readf(const char *filename)
 {
+	char *text = NULL;
+	size_t size = 0;
+	ssize_t fd;
+
+	fd = open(filename, O_RDONLY);
 }
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * main - interpreter for Monty ByteCodes files
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+
+	return (0);
+}
