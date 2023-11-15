ab81cea6105b4392819e290a508a4ec881633982
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-15 13:01:27 -0800

    0

--- file_func.c
+++ file_func.c
@@ -62,17 +62,3 @@ void exec(ssize_t fd, line,unsigned int l, stack_t **stack)
 		exit(EXIT_FAILURE);
 	}
 }
-/**
- * pushf - pushes an element to the stack
- * @stack: pointer to the stack
- * @line_number: line number
- */
-void pushf(stack_t **stack, unsigned int line_number)
-{
-	unsigned int i = 0;
-
-	if (!(data.arg))
-	{
-		dprintf(STDERR_FILENO, "L%i: usage: push integer", data.line);
-	}
-}
--- opcodes_1.c
+++ opcodes_1.c
@@ -1,1 +1,15 @@
-/* This is where we're gonna put our functions for opcodes */
+#include "monty.h"
+/**
+ * pushf - pushes an element to the stack
+ * @stack: pointer to the stack
+ * @line_number: line number
+ */
+void pushf(stack_t **stack, unsigned int line_number)
+{
+	unsigned int i = 0;
+
+	if (!(data.arg))
+	{
+		dprintf(STDERR_FILENO, "L%i: usage: push integer", data.line);
+	}
+}
